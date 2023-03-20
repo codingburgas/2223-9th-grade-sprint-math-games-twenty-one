68a960bf6a6cdf2393781ef405e3e53228108ce2
IDDochev21 <IDDochev21@codingburgas.bg>
2023-03-21 00:45:11 +0200

    Add sprite and movement

--- 2223-9th-grade-sprint-math-games/2223-9th-grade-sprint-math-games.cpp
+++ 2223-9th-grade-sprint-math-games/2223-9th-grade-sprint-math-games.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main()
 {
+    SetTargetFPS(60);
+
     // Set up the screen parameters
     const int screenWidth = 1000;
     const int screenHeight = 550;
@@ -13,11 +15,32 @@ int main()
     const int gridSize = 50;
     const Color gridColor = LIGHTGRAY;
 
+    // Set up the player's starting position
+    Rectangle player = { screenWidth / 2 - 20, screenHeight / 2 - 20, 50, 50 }; 
+
     // Main game loop
     while (!WindowShouldClose())
     {
 
+        if (IsKeyDown(KEY_LEFT))
+        {
+            player.x -= 10;
+        }
+        if (IsKeyDown(KEY_RIGHT))
+        {
+            player.x += 10;
+        }
+        if (IsKeyDown(KEY_UP))
+        {
+            player.y -= 10;
+        }
+        if (IsKeyDown(KEY_DOWN))
+        {
+            player.y += 10;
+        }
+
         BeginDrawing();
+
         ClearBackground(RAYWHITE);
 
         // Draw horizontal lines
@@ -25,13 +48,12 @@ int main()
         {
             DrawLine(0, i, screenWidth, i, gridColor);
         }
-
         // Draw vertical lines
         for (int i = 0; i < screenWidth; i += gridSize)
         {
             DrawLine(i, 0, i, screenHeight, gridColor);
         }
-
+        DrawRectangleRec(player, BLUE);
         EndDrawing();
     }
 
