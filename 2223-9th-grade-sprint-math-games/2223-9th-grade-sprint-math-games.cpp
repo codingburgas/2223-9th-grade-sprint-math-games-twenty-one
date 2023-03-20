9f78ebf0b98a3ce82464d9f3500bbdf5b674d927
IDDochev21 <IDDochev21@codingburgas.bg>
2023-03-20 22:54:47 +0200

    Add grid

--- 2223-9th-grade-sprint-math-games/2223-9th-grade-sprint-math-games.cpp
+++ 2223-9th-grade-sprint-math-games/2223-9th-grade-sprint-math-games.cpp
@@ -2,14 +2,40 @@
 #include <raylib.h>
 using namespace std;
 
-//Menu function
 int main()
 {
-    InitWindow(800, 800, "twenty-one");
-    while(!WindowShouldClose())
+    // Set up the screen parameters
+    const int screenWidth = 1000;
+    const int screenHeight = 550;
+    InitWindow(screenWidth, screenHeight, "twenty-one");
+
+    // Set up the grid parameters
+    const int gridSize = 50;
+    const Color gridColor = LIGHTGRAY;
+
+    // Main game loop
+    while (!WindowShouldClose())
     {
+
         BeginDrawing();
-        DrawText("twenty-one", 240, 350, 10, DARKGRAY);
+        ClearBackground(RAYWHITE);
+
+        // Draw horizontal lines
+        for (int i = 0; i < screenHeight; i += gridSize)
+        {
+            DrawLine(0, i, screenWidth, i, gridColor);
+        }
+
+        // Draw vertical lines
+        for (int i = 0; i < screenWidth; i += gridSize)
+        {
+            DrawLine(i, 0, i, screenHeight, gridColor);
+        }
+
         EndDrawing();
     }
+
+    CloseWindow();
+
+    return 0;
 }
