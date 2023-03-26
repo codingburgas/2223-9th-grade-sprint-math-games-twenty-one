10ebb5b641eee7f27042227920c4fe0b174f2483
TYAtanasov21 <TYAtanasov21@codingburgas.bg>
2023-03-26 11:57:33 +0300

    Modify binary multiplication function

--- 2223-9th-grade-sprint-math-games/2223-9th-grade-sprint-math-games.cpp
+++ 2223-9th-grade-sprint-math-games/2223-9th-grade-sprint-math-games.cpp
@@ -3,6 +3,10 @@
 
 using namespace std;
 
+//Prototypes
+int addGame(int, int);
+
+
 // Set up the screen parameters
 const int screenWidth = 400;
 const int screenHeight = 300;
@@ -17,10 +21,12 @@ unsigned int binNum[3][5] = {
 
 void multGame()
 {
+
     int firstRandomNum = 0, secondRandomNum = 0;
-    //int multiply = 0, factor = 1;
+
     cout << "\nWelcome! Now you have multiply the first binary number that you see with the second one on your screen!" << endl;
     cout << "Input your answear after the equals sign." << endl;
+
     for (int i = GetRandomValue(0, 4); i < 3; i++)
     {
         for (int j = GetRandomValue(0, 4); j < 5; j++)
@@ -29,67 +35,102 @@ void multGame()
             secondRandomNum = binNum[i][j - 1];break;
         }    
     }
+
     cout << firstRandomNum << " x " << secondRandomNum << " = ";
     int userInput;
     cin >> userInput;
 
+    int result = 0, factor = 1;
+    while (secondRandomNum != 0)
+    {
+        int digit = secondRandomNum % 10;
+        if (digit == 1)
+        {
+            firstRandomNum = firstRandomNum * factor;
+            result = addGame(firstRandomNum, result);
+        }
+        else
+        {
+            firstRandomNum = firstRandomNum * factor;
+        }
+        secondRandomNum = secondRandomNum / 10;
+        factor = 10;
+    }
 
 
-    //while (secondRandomNum != 0)
-    //{
-    //    digit = secondRandomNum % 10;
-    //    if (digit == 1)
-    //    {
-    //        firstRandomNum = firstRandomNum * factor;
-    //        multiply = binaryproduct(firstRandomNum, multiply);
-    //    }
-    //    else
-    //        binary1 = binary1 * factor;
-    //    binary2 = binary2 / 10;
-    //    factor = 10;
-    //}
+    if (userInput == result)
+    {
+        cout << "Your answear is correct! Congratulations!" << endl;
+    }
+    else
+    {
+        cout << "The answear is incorrect! Try again!" << endl;
+        multGame();
+    }
 
 }
 
 void subGame()
 {
-
+    cout << "You are playing subGame" << endl;
 }
 
-void addGame()
+int addGame(int binary1, int binary2)
 {
+    cout << "You are playing addition" << endl;
+    int i = 0, remainder = 0, sum[20];
+    int binaryprod = 0;
 
+    while (binary1 != 0 || binary2 != 0)
+    {
+        sum[i++] = (binary1 % 10 + binary2 % 10 + remainder) % 2;
+        remainder = (binary1 % 10 + binary2 % 10 + remainder) / 2;
+        binary1 = binary1 / 10;
+        binary2 = binary2 / 10;
+    }
+    if (remainder != 0)
+        sum[i++] = remainder;
+    --i;
+    while (i >= 0)
+        binaryprod = binaryprod * 10 + sum[i--];
+    return binaryprod;
 }
 
 void divGame()
 {
-
+    cout << "You are playing division" << endl;
 }
 
 //Move function
 void move(Vector2 &position)
 {
+    //Move left
     if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))
     {
         if(position.x > 0)
             position.x -= 10;
     }
+    //Move right
     if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D))
     {
         if(position.x < screenWidth - 90)
             position.x += 10;
     }
+    //Move up
     if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W))
     {
         if (position.y > 0)
             position.y -= 10;
     }
+    //Move down
     if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S))
     {
         if(position.y < screenHeight - 75)
             position.y += 10;
     }
 }
+
+
 //Main function
 int main()
 {
@@ -104,9 +145,16 @@ int main()
     Image icon = LoadImage("images/icon.png");
     SetWindowIcon(icon);
 
-    //Rectangles
+    //Grid layout
     Rectangle rec[3][4];
     bool flagDiv = false, flagMult = false, flagSub = false, flagAdd = false;
+    const char* level[3][4] = {
+    "-", "+", "/", "-",
+    "x", "-", "+", "x",
+    "-", "+", "/", "x",
+    };
+
+
     //Main game loop
     while (!WindowShouldClose())
     {
@@ -114,7 +162,7 @@ int main()
         BeginDrawing();
         
         ClearBackground(RAYWHITE);
-    
+    //Draw rectangle grid layout
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 4; j++)
@@ -124,12 +172,7 @@ int main()
                 DrawRectangleLinesEx(rec[i][j], 1, BLACK);
             }
         }
-
-        const char* level[3][4] = {
-        "-", "+", "/", "-",
-        "x", "-", "+", "x",
-        "-", "+", "/", "x",
-        };
+    //Draw mathematical symbols
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 4; j++)
@@ -137,6 +180,7 @@ int main()
                 DrawText(level[i][j], rec[i][j].x + (screenWidth / 4) / 2 - 15, rec[i][j].y + (screenHeight / 3) / 2 - 60 / 2, 60, BLUE);
             }
         }
+        //Check if the enter key is pressed when over a symbol
         if (IsKeyDown(KEY_ENTER))
         {
             for (int i = 0; i < 3; i++)
@@ -147,23 +191,39 @@ int main()
                     {
                         if (level[i][j] == "-")
                         {
-                            subGame();
+                            flagSub = true;
                         }
                         else if (level[i][j] == "+")
                         {
-                            addGame();
+                            flagAdd = true;
                         }
                         else if (level[i][j] == "x")
                         {
-                            multGame();
+                            flagMult = true;
                         }
                         else if (level[i][j] == "/")
                         {
-                            divGame();
+                            flagDiv = true;
                         }
                     }
                 }
             }
+        }
+        //Call functions for every different level
+        if (flagMult)
+        {
+            multGame();
+            flagMult = false;
+        }
+        if (flagSub)
+        {
+            subGame();
+            flagSub = false;
+        }
+        if (flagDiv)
+        {
+            divGame();
+            flagDiv = false;
         }
 
         DrawTexture(mainCharacter, position.x, position.y, WHITE);
